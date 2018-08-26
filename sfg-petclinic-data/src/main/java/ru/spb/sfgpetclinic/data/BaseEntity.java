4b02ac226497dcc4283c16f6af698f55b0f19ef5
avetall87 <avetall87@gmail.com>
2018-08-26 16:42:01 +0300

    added BaseEntity. close #3

---
+++ sfg-petclinic-data/src/main/java/ru/spb/sfgpetclinic/data/BaseEntity.java
@@ -0,0 +1,18 @@
+package ru.spb.sfgpetclinic.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public boolean isNew(){
+        return this.id == null;
+    }
+
+}
