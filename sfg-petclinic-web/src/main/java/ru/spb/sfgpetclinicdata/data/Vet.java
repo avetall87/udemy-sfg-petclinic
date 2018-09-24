1511b4f17c9ef534d9356b63dba86cdd415ab22b
avetall87 <avetall87@gmail.com>
2018-09-24 23:45:56 +0300

    added owners layout. close #19

--- sfg-petclinic-web/src/main/java/ru/spb/sfgpetclinicdata/data/Vet.java
+++ sfg-petclinic-web/src/main/java/ru/spb/sfgpetclinicdata/data/Vet.java
@@ -1,5 +1,26 @@
 package ru.spb.sfgpetclinicdata.data;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
 public class Vet extends Person {
+    private Set<String> specialties;
+
+    protected Set<String> getSpecialtiesInternal() {
+        if (this.specialties == null) {
+            this.specialties = new HashSet<>();
+        }
+        return this.specialties;
+    }
+
+    public int getNrOfSpecialties() {
+        return getSpecialtiesInternal().size();
+    }
+
 }
